d1cf0d693608fffccbc7323f2d3654ef03efbf29
tal <tamen98@gmail.com>
2021-02-20 17:00:20 +0200

    Autogenerate menu and rotating images, remove unnecessary code

---
+++ recipes_data.js
@@ -0,0 +1,37 @@
+// names
+var starters = [
+	"Avocado with Eggs",
+	"Nachos"
+];
+
+var mains = [
+	"Bagel",
+	"Kube Selek"
+];
+
+var desserts = [
+	"Oznei Haman",
+	"Pistachio Chocolate Chip Cookies",
+	"Oatmeal Pancakes",
+	"Sahlav",
+	"Tarte Tatin",
+	"Sticky Rice with Mango",
+	"Almond Cookies",
+	"Raisin Danish Pastry"
+];
+
+// images
+var starter_images = [
+	"avocado-with-eggs.jpg",
+	"nachos.jpg"
+];
+var main_images = [
+	"bagel.jpg",
+	"kube-selek.jpg"
+];
+var dessert_images = [
+	"oznei-haman.jpg",
+	"pistacio.jpg",
+	"oatmeal-pancakes.jpg",
+	"oatmeal-chocolate-chips.jpg",
+];
